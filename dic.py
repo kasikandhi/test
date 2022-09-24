461360ae1541cf115bd9fa91853edfe2e7df6e96
kasikandhi <kasikandhi@gmail.com>
2022-09-24 20:30:29 +0530

    new

---
+++ dic.py
@@ -0,0 +1,10 @@
+d={"name":"king","age":15,"percentage":90.13}
+print(d)
+print(d["age"])
+f={"name":"king",15:24,15.60:7838,True:True}
+print(len(f))
+print(d.get('name'))
+d.update({"name":"vaihu"})
+print(d)
+print(d.keys())
+print(d.values())
