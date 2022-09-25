af1be4f916c2c6412cf9c5e1850a9c71894d0798
kasikandhi <113297858+kasikandhi@users.noreply.github.com>
2022-09-25 12:54:52 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.util.*;
+public class Demo{
+public static void main(String []args){
+    int a =100;
+System.out.println("Hello World");
+System.out.println(a);
+name();
+}
+static void name(){
+    System.out.println("my name is kasi");
+//this is comment
+}
+}
